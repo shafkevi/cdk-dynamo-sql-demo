10c7aaa99dbe2bf56a9ef4afbe685a4291f8341f
Mathew Werber <matwerber@gmail.com>
2021-04-22 08:40:45 -0700

    remove hard-coded vpc id

--- lib/stacks/TemplateStack.ts
+++ lib/stacks/TemplateStack.ts
@@ -5,8 +5,8 @@ export default class Template extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     new TemplateConstruct(this, id, { 
-      vpcId: 'vpc-0a2cad50c98aed83f'    // optional param to use existing VPC; if not specified, will use default VPC.
-      //userArn: 'xxxx'                 // optional, specify a user/role ARN that can access the Cloud9 instance; if not specified, only the IAM user/role running CDK can access the Cloud9
+       //vpcId: 'vpc-xxx'         // optional param to use existing VPC; if not specified, will use default VPC.
+      //userArn: 'xxxx'           // optional, specify a user/role ARN that can access the Cloud9 instance; if not specified, only the IAM user/role running CDK can access the Cloud9
     });
   }
 }
